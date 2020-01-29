c263e892b7ab3ca5ea878f02bd3c3c119830e344
MohammadTrainingNassar <Mohammad@Nassar.com>
2020-01-29 16:11:06 +0200

    worked

--- ShoppingCart/src/main/java/com/training/ShoppingCart/adapter/message/outbound/CartoutputChannel.java
+++ ShoppingCart/src/main/java/com/training/ShoppingCart/adapter/message/outbound/CartoutputChannel.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.messaging.MessageChannel;
 
 public interface CartoutputChannel {
-	String CHANNEL_NAME = "user-events";
+	String CHANNEL_NAME = "cart-events";
 
 	@Output(CHANNEL_NAME)
 	MessageChannel outChannel();
